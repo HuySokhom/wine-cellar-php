88f24d610ee07e28602e6a112e5de17fd5837a63
HuySokhom <kom.huy@gmail.com>
2016-04-07 18:48:07 +0700

    update connection

--- api/index.php
+++ api/index.php
@@ -121,7 +121,7 @@ function getConnection() {
 	$dbhost="127.0.0.1";
 	$dbuser="root";
 	$dbpass="";
-	$dbname="imi";
+	$dbname="ng";
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbh;
